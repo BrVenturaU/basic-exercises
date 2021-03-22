6701d77491f2e58f33b62fd2bf859c26b2df3c71
BrVenturaU <brandonventura16@gmail.com>
2021-03-21 21:34:32 -0600

    Agrega estilos a ejercicio 4.

--- pages/exercise-4.php
+++ pages/exercise-4.php
@@ -37,56 +37,72 @@
         ),
     );
 ?>
-
-    <h1>4. Cree un array asociativoen donde almacene los datos mostrados en la siguiente tabla:</h1>
-    <table border="1">
-        <thead>
-            <tr>
-                <th>Departamento</th>
-                <th>Empleado</th>
-                <th>Salario</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php 
-                $salario=0.0;
-                foreach($empleados as $indices=>$datosEmpleado){
-                    
-                    echo "<tr>";
-                        foreach($datosEmpleado as $clave=>$dato){
-                            echo $clave == "salario" ? "<td>$$dato</td>" : "<td>$dato</td>";
-                            $salario += $clave == "salario" ? $dato : 0;
-                        } 
-                    echo "</tr>";
-                }
-                $salarioPromedio = round($salario / count($empleados), 2);
-            ?>
-            <tr>
-                <td colspan="2">Salario Total</td>
-                <td>$<?=$salario?></td>
-            </tr>
-            <tr>
-                <td colspan="2">Salario Promedio</td>
-                <td>$<?=$salarioPromedio?></td>
-            </tr>
-        </tbody>
-        <tfoot>
-            <tr>
-                <th>Departamento</th>
-                <th>Empleado</th>
-                <th>Salario</th>
-            </tr>
-        </tfoot>
-    </table>
-
-    <div>
-        <dl>
-            <?php
-                $empleadosDepartamento = array_count_values(array_column($empleados, "departamento"));
-                foreach($empleadosDepartamento as $departamento=>$total){
-                    echo "<dt>Empleados en: $departamento.</dt>";
-                    echo "<dd>$total</dd>";
-                }
-            ?>
-        </dl>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1>4. Cree un array asociativo en donde almacene los datos mostrados en la siguiente tabla:</h1>
+        </div>
     </div>
+    <div class="row">
+        <div class="col-12">
+            <table class="table lead table-dark table-striped table-bordered table-hover ">
+                <thead>
+                    <tr>
+                        <th>Departamento</th>
+                        <th>Empleado</th>
+                        <th>Salario</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php 
+                        $salario=0.0;
+                        foreach($empleados as $indices=>$datosEmpleado){
+                            
+                            echo "<tr>";
+                                foreach($datosEmpleado as $clave=>$dato){
+                                    echo $clave == "salario" ? "<td>$$dato</td>" : "<td>$dato</td>";
+                                    $salario += $clave == "salario" ? $dato : 0;
+                                } 
+                            echo "</tr>";
+                        }
+                        $salarioPromedio = round($salario / count($empleados), 2);
+                    ?>
+                    <tr>
+                        <td colspan="2">Salario Total</td>
+                        <td>$<?=$salario?></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">Salario Promedio</td>
+                        <td>$<?=$salarioPromedio?></td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th>Departamento</th>
+                        <th>Empleado</th>
+                        <th>Salario</th>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12 col-sm-6 mx-auto">
+            <ul class="list-group">
+                <?php
+                    $empleadosDepartamento = array_count_values(array_column($empleados, "departamento"));
+                    foreach($empleadosDepartamento as $departamento=>$total){
+                        echo "
+                            <li class='list-group-item d-flex justify-content-between align-items-center'>
+                                Empleados en: <strong>$departamento</strong>.
+                                <span class='badge badge-primary badge-pill'>$total</span>
+                            </li>
+                        
+                        ";
+                    }
+                ?>
+            </ul>
+        </div>
+    </div>
+</div>
