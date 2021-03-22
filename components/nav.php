0fd1f6f945d51216cd85e48004f3b833822dafeb
BrVenturaU <brandonventura16@gmail.com>
2021-03-21 21:20:32 -0600

    Modifica navbar.

--- components/nav.php
+++ components/nav.php
@@ -1,38 +1,22 @@
-<style>
-    *{
-        padding: 0;
-        margin: 0;
-        box-sizing: border-box;
-    }
-    nav{
-        background-color: black;
-        height: 3.5em;
-        display: flex;
-        margin-bottom: 2em;
-    }
-    nav a {
-        padding: 16px;
-        background-color: gray;
-        color: white;
-        text-decoration: none;
-        transition: color 0.5s ease;
-        transition: background-color 0.5s ease;
-    }
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">P-IV</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-    nav a:hover{
-        background-color: white;
-        color: gray;
-    }
-</style>
-
-<nav>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
     <?php
         $fileList = glob('pages/*');
         foreach($fileList as $index=>$file){
             $file = str_replace("pages/", "", $file);
             echo "
-                <a href='$file'>Ejercicio ".($index+1)."</a>
+                <li class='nav-item'>
+                    <a class='nav-link' href='$file'>Ejercicio ".($index+1)."</a>
+                </li>
             ";
         }
     ?>
+    </ul>
+  </div>
 </nav>
